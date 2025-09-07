d749a819333dbed2c03b7f6f44c11cd0af515f60
nik-kubala <nikolas.kubala81@gmail.com>
2025-09-07 16:48:01 +0200

    mam dost

---
+++ Škola-hodiny/Precvičovanie/Codewars/Consecutive strings.py
@@ -0,0 +1,10 @@
+def longest_consec(strarr, k):
+    zoznam = []
+    if len(strarr) == 0 or k > len(strarr) or k <= 0:
+        return ""
+    else:
+        for i in range(0, len(strarr) - k + 1):
+            zoznam.append("".join(strarr[i : i + k]))
+    for 
+    print(zoznam)
+longest_consec(["tree", "foling", "trashy", "blue", "abcdef", "uvwxyz"], 2)
